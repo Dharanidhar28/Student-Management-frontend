6ae110fab124153116a0cd3813540966dd77d2b2
Dharanidhar28 <dharanidharkotha@gmail.com>
2026-03-23 00:27:26 +0530

    feat: improve dashboard UX with auto-closing form and delete confirmation

--- api_helper.js
+++ api_helper.js
@@ -2,19 +2,18 @@ const API_BASE = "http://127.0.0.1:8000";
 const token = localStorage.getItem("token");
 
 export async function apiRequest(endpoint, options = {}) {
+	const res = await fetch(API_BASE + endpoint, {
+		...options,
 
-const res = await fetch(API_BASE + endpoint, {
+		headers: {
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`,
+			...options.headers,
+		},
+	});
+	if (!res.ok) {
+		throw new Error(`API request failed with status ${res.status}`);
+	}
 
-...options,
-
-headers: {
-"Content-Type": "application/json",
-"Authorization": `Bearer ${token}`,
-...options.headers
-}
-
-});
-
-return res.json();
-
+	return res.json();
 }
